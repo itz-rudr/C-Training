54ec8a9db0623ee60f03cd3c03f127ac98774aae
itz-rudr <rudrag26.2@gmail.com>
2025-09-17 21:41:31 +0530

    Pattern printing

---
+++ lec10/1st.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter number of repetitions: ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            printf("*");
+        }
+        for(int k=n-1;k>i;k--){
+            printf(" ");
+        }
+        for(int l=n-1;l>i;l--){
+            printf(" ");
+        }
+        for(int j=0;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
---
+++ lec10/2nd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter number of repetitions: ");
+    scanf("%d",&n);
+    for(int j=0;j<=i;j++){
+            printf("*");
+        }
+        for(int k=n-1;k>i;k--){
+            printf(" ");
+        }
+}  
+int mai { }
